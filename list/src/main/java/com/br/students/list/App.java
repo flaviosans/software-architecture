496ef38a6b3457d648bb6e00366c35f03417286e
Flávio Santos <flaviosantos.dev@gmail.com>
2020-03-12 22:46:19 -0300

    Arrow functions, melhora no nome das variáveis

--- list/src/main/java/com/br/students/list/App.java
+++ list/src/main/java/com/br/students/list/App.java
@@ -9,41 +9,49 @@ import java.util.Scanner;
  */
 public class App {
     private static Scanner scanner;//foi declarado o método que irá ser utilizado no projeto no inicio da classe
-    private static List<String> listaUsuarios;//foi mudado o nome da variavel, para o código ser mais legivel
+    private static List<String> listStudents;//foi mudado o nome da variavel, para o código ser mais legivel
     private static boolean run;
+    private static String option,name;
 
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
-        listaUsuarios = new ArrayList<String>();
+        listStudents = new ArrayList<String>();
         run = true;
 
         while (run) {
-            System.out.println("Menu");
-            System.out.println("Digite 1 para inserir o estudante ");
-            System.out.println("Digite 2 para imprimir a lista ");
-
-            String option = scanner.nextLine();
-
+            exibeMenu(); // Quebrando o código em métodos menores
             if (option.equals("1")) {
-                System.out.println("Digite o nome do aluno: ");
-
-                String name = scanner.nextLine();
-                if (!listaUsuarios.contains(name)) {
-                    listaUsuarios.add(name);
-                } else {
-                    System.out.println("O aluno já esta cadastrado!");
-                }
-
+                cadastraUsuario(); // Quebrando o código em métodos menores
             } else if (option.equals("2")) {
-                System.out.println("Listando ...");
-                for (String student : listaUsuarios) {
-                    System.out.println(student);
-                }
+                listaUsuarios(); // Quebrando o código em métodos menores
                 run = false;
             } else {
                 System.out.println("Digite uma opção válida");
             }
         }
         scanner.close();
+    }
+
+    private static void exibeMenu() {
+        System.out.println("Menu");
+        System.out.println("Digite 1 para inserir o estudante ");
+        System.out.println("Digite 2 para imprimir a lista ");
+
+        option = scanner.nextLine();
+    }
+
+    private static void listaUsuarios(){
+        System.out.println("Listando ...");
+        listStudents.forEach(System.out::println); // Trocando loop por lambda Function (arrow function)
+    }
+
+    private static void cadastraUsuario(){
+        System.out.println("Digite o nome do aluno: ");
+        name = scanner.nextLine();
+        if (!listStudents.contains(name)) {
+            listStudents.add(name);
+        } else {
+            System.out.println("O aluno já esta cadastrado!");
+        }
     }
 }
